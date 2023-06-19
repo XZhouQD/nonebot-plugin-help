d8efb68892ba177949a17c9e1e948a13c5da4a93
renmu123 <1101022351@qq.com>
2023-06-19 14:23:54 +0800

    update to_me,ignore_plugins config to help_to_me,help_ignore_plugins

--- nonebot_plugin_help/config.py
+++ nonebot_plugin_help/config.py
@@ -6,5 +6,5 @@ class Config(BaseModel, extra=Extra.ignore):
     # Your Config Here
     help_block: Optional[bool] = False
     help_priority: Optional[int] = 1
-    ignore_plugins: Optional[list] = []
-    to_me: Optional[bool] = False
+    help_ignore_plugins: Optional[list] = []
+    help_to_me: Optional[bool] = False
--- nonebot_plugin_help/handler.py
+++ nonebot_plugin_help/handler.py
@@ -13,7 +13,7 @@ default_start = command_starts[0]
 plugin_config = Config.parse_obj(nonebot.get_driver().config)
 
 helper = on_command("help", priority=plugin_config.help_priority, aliases={"帮助"}, block=plugin_config.help_block,
-                    rule=to_me() if plugin_config.to_me else None)
+                    rule=to_me() if plugin_config.help_to_me else None)
 # Matcher level info registering, still active in-use
 helper.__help_name__ = 'help'
 helper.__help_info__ = f'''{default_start}help  # 获取本插件帮助
@@ -41,7 +41,7 @@ async def get_result(event: Event, content: Message = Arg()):
         plugin_set = nonebot.plugin.get_loaded_plugins()
         plugin_names = []
         for plugin in plugin_set:
-            if plugin.name in plugin_config.ignore_plugins:
+            if plugin.name in plugin_config.help_ignore_plugins:
                 continue
             # plugin.name, then metadata name or legacy help name
             name = f'{plugin.name} | '
